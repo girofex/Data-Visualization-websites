3d1a6c92c2741ee204ab96ed4e9a9a22438630d6
Federica Tamerisco <f.tamerisco@gmail.com>
2025-10-30 12:41:48 +0100

    scrollbar and separate css

--- js/theme.js
+++ js/theme.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.querySelector(".navbar");
     var body = document.body;
 
+    var scrollbarThumbBorderColor = "#102542";
+    document.documentElement.style.setProperty("--scrollbar-thumb-border-color", scrollbarThumbBorderColor);
+
     var themeMode = localStorage.getItem("theme") || "dark";
     applyTheme(themeMode);
 
@@ -19,11 +22,17 @@ document.addEventListener("DOMContentLoaded", function() {
         navbar.classList.toggle("navbar-mode", isDark);
 
         if (isDark) {
+            mode.style.color = "#ebe7e6";
+            scrollbarThumbBorderColor = "#ebe7e6";
             mode.classList.remove("bi-brightness-high-fill");
             mode.classList.add("bi-moon-fill");
         } else {
+            mode.style.color = "#102542";
+            scrollbarThumbBorderColor = "#102542";
             mode.classList.remove("bi-moon-fill");
             mode.classList.add("bi-brightness-high-fill");
         }
+
+        document.documentElement.style.setProperty("--scrollbar-thumb-border-color", scrollbarThumbBorderColor);
     }
 });
